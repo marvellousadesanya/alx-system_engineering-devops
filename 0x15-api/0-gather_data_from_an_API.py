fd5ab3dd3b90b753e085f4c9bf523ab8098b3149
Marv Adesanya <marvellousniyi123@gmail.com>
2023-02-08 14:52:40 +0000

    I showed up. Small win. Big progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Using a public API 'https://jsonplaceholder.typicode.com'"""
+import requests
+import sys
+
+# base url of the api
+baseUrl = f"https://jsonplaceholder.typicode.com"
+
+def get_employee_todo_list_progress(employee_id):
+    # get employee name
+    employee_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    employee_name = employee_response.json().get("name")
+
+    # get employee todo list
+    todo_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    todo_list = todo_response.json()
+
+    # count number of completed and total tasks
+    completed_tasks = [task for task in todo_list if task["completed"]]
+    total_tasks = len(todo_list)
+    completed_tasks_count = len(completed_tasks)
+
+    # print progress
+    print(f"Employee {employee_name} is done with tasks({completed_tasks_count}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t {task['title']}")
+
+# test with employee id 1
+get_employee_todo_list_progress(sys.argv[1])
+
+
+# print(response.content)
